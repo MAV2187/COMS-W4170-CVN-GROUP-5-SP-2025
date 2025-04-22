c5e352331122b5a6c2ba60193372efbada60f211
MAV2187 <mav2187@columbia.edu>
2025-04-22 01:27:43 -0700

    Updated Title

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,5 @@
 // Global application scripts can be added here
-console.log("3D Modeling Tutor application loaded");
+console.log("3D Conceptualization Using Basic Primitives & Swept 2D Profiles application loaded");
 
 // Additional interactive elements can be initialized here
 $(document).ready(function() {
